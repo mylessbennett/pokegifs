5d51b64e4e3d9a40cc7816051a22ffaf638e39b7
Myles Bennett <myles.bennett@hotmail.com>
2019-03-21 16:40:38 -0400

    added gif url of pokemon to json repsonse

--- pokegifs/pokegifs/views.py
+++ pokegifs/pokegifs/views.py
@@ -1,6 +1,9 @@
 import requests
 import json
+import os
 from django.http import JsonResponse
+
+key = os.environ.get("GIPHY_KEY")
 
 
 def pokemon_view(request, id):
@@ -12,8 +15,16 @@ def pokemon_view(request, id):
     for type in body['types']:
         types.append(type["type"]["name"])
 
+    gif_url = "http://api.giphy.com/v1/gifs/search?api_key={}&q={}".format(key, name)
+    print(gif_url)
+    gif_res = requests.get(gif_url)
+    gif_body = json.loads(gif_res.content)
+    gif = gif_body['data'][0]['url']
+
+
     return JsonResponse({
         "name": name,
         "id": id,
         "types": types,
+        "gif": gif,
     })
